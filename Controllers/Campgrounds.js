7b3ea95c7a4271bad2a5458466463b86de2bf4ec
Rujul Singh <rujulsingh1@gmail.com>
2021-12-09 03:34:48 -0500

    Bug fixes

--- Controllers/Campgrounds.js
+++ Controllers/Campgrounds.js
@@ -54,6 +54,7 @@ module.exports.editCampground = async (req, res) => {
 module.exports.createCampground = async (req, res, next) => {
     const newCamp = new Campground(req.body.campground);
     newCamp.image = req.files.map(f => ({ url: f.location, key: f.key }));
+    console.log(req.files);
     newCamp.owner = req.user;
     await newCamp.save();
     req.flash('success', 'Successfully created a new campground');
