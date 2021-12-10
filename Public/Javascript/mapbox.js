3f06b7ece2460cd25846664f538eac00b1ef803a
Rujul Singh <rujulsingh1@gmail.com>
2021-12-10 02:47:42 -0500

    Added sample clustermap to the home page

--- Public/Javascript/mapbox.js
+++ Public/Javascript/mapbox.js
@@ -1,11 +1,12 @@
 mapboxgl.accessToken = MAP_TOKEN;
 const map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
-  center: currCampground.coordinates, // starting position [lng, lat]
+  style: "mapbox://styles/mapbox/satellite-v9",
+  center: currCampground.geometry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
 
 const marker = new mapboxgl.Marker()
-  .setLngLat(currCampground.coordinates)
+  .setLngLat(currCampground.geometry.coordinates)
+  .setPopup(new mapboxgl.Popup().setHTML(`<h4>${currCampground.title}</h4>`)) // add popup
   .addTo(map);
