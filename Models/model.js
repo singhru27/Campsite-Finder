98089739f7755dc502ca9ce59c72ae994092c0fa
Rujul Singh <rujulsingh1@gmail.com>
2021-12-11 19:10:53 -0500

    Added links on clustermap page

--- Models/model.js
+++ Models/model.js
@@ -1,44 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Review = require("./review.js");
 
-const CampgroundSchema = mongoose.Schema({
+const CampgroundSchema = mongoose.Schema(
+  {
     title: String,
     price: Number,
     description: String,
-    image: [{
+    image: [
+      {
         url: String,
-        key: String
-    }],
+        key: String,
+      },
+    ],
     location: String,
     owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
     geometry: {
-        type: {
-          type: String, // Don't do `{ location: { type: String } }`
-          enum: ['Point'], // 'location.type' must be 'Point'
-          required: true
-        },
-        coordinates: {
-          type: [Number],
-          required: true
-        }
+      type: {
+        type: String, // Don't do `{ location: { type: String } }`
+        enum: ["Point"], // 'location.type' must be 'Point'
+        required: true,
       },
-    reviews: [{
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    reviews: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Review"
-    }]
-})
+        ref: "Review",
+      },
+    ],
+  },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
+
+CampgroundSchema.virtual("properties.popup").get(function () {
+  return `<a href=/campgrounds/${this._id}> ${this.title} </a>`;
+});
 
 CampgroundSchema.post("findOneAndDelete", async (doc) => {
-    if (doc) {
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews
-            }
-        });
-    }
-
-})
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
 module.exports = mongoose.model("Campground", CampgroundSchema);
--- Public/Javascript/clusterMap.js
+++ Public/Javascript/clusterMap.js
@@ -93,8 +93,7 @@ map.on("load", () => {
   // description HTML from its properties.
   map.on("click", "unclustered-point", (e) => {
     const coordinates = e.features[0].geometry.coordinates.slice();
-    const mag = e.features[0].properties.mag;
-    const tsunami = e.features[0].properties.tsunami === 1 ? "yes" : "no";
+    const currHTML = e.features[0].properties.popup;
 
     // Ensure that if the map is zoomed out such that
     // multiple copies of the feature are visible, the
@@ -103,10 +102,7 @@ map.on("load", () => {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
-    new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML(`magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`)
-      .addTo(map);
+    new mapboxgl.Popup().setLngLat(coordinates).setHTML(currHTML).addTo(map);
   });
 
   map.on("mouseenter", "clusters", () => {
